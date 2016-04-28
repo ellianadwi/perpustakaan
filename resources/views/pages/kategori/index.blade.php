693a8c6692abeb684ca7abf0c43989cf582f9758
ellianadwi <ellianadwi26@gmail.com>
2016-04-28 14:21:18 +0700

    refisi fitur kategori

--- resources/views/pages/kategori/index.blade.php
+++ resources/views/pages/kategori/index.blade.php
@@ -77,20 +77,38 @@
         </div>
     </div>
     <div id="Create">
-
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Tambah Kategori
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-8">
-                                <form id="Form-Create" class="form-horizontal form-label-left">
-                                    <div class="form-group">
-                                        <label class="control-label col-sm-3">
-                                            Kode Kategori </label>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_title">
+                    <h2>Tambah Kategori</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Settings 1</a>
+                                </li>
+                                <li><a href="#">Settings 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a class="close-link"><i class="fa fa-close"></i></a>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-lg-8">
+                                        <form id="Form-Create" class="form-horizontal form-label-left">
+                                            <div class="form-group">
+                                                <label class="control-label col-sm-3">
+                                                Kode Kategori </label>
                                         <div class="col-sm-6">
                                             <input type="text" name="kode_kategori" id="kode_kategori"
                                                    required="required" class="form-control col-md-12 col-xs-12">
@@ -121,7 +139,7 @@
             </div>
         </div>
     </div>
-
+</div></div>
     <div id="Edit">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
